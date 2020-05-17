16a53a1060e8fd1cae6c4f400a412800ca215cf3
Ethan <ethan.hargunani@outlook.com>
2020-05-17 13:46:42 -0700

    changed wording of variables

---
+++ variables/target/rls/debug/deps/variables-e14be62a6d58312c.d
@@ -0,0 +1,5 @@
+/home/ethan/github/LearningRust/variables/target/rls/debug/deps/variables-e14be62a6d58312c.rmeta: src/main.rs
+
+/home/ethan/github/LearningRust/variables/target/rls/debug/deps/variables-e14be62a6d58312c.d: src/main.rs
+
+src/main.rs:

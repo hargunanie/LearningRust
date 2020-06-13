b2efe08fef39f2d55c72461b3c35d15de793129a
Ethan <ethan.hargunani@outlook.com>
2020-06-13 12:11:43 -0700

    changed main.rs

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,4 @@
-use rand::Rng;
-
 fn main() {
-    println!("Hello world!");
-    let randnum = rand::thread_rng().gen_range(1, 1001);
-    println!("The random number is: {}", randnum)
+    let a = [3; 5];
+    println!(a.2);
 }

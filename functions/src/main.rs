44427edfea67a54e6126b5113ef1779153af7303
Ethan <ethan.hargunani@outlook.com>
2020-07-09 20:00:37 -0700

    cargo

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,9 @@
-fn five() -> i32 {
-    5
+fn main() {
+    let x = plus_one(-24);
+    
+    println!("The value of x is: {}", x);
 }
 
-fn main() {
-    let x = five();
-
-    println!("The value of x is: {}", x);
+fn plus_one(x: i32) -> i32 {
+    x+1
 }

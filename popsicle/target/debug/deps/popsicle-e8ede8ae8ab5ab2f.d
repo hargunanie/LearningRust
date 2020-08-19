b65e18409ae64f91437c4ba5ec474d1850633b39
Ethan <ethan.hargunani@outlook.com>
2020-08-19 15:06:31 -0700

    cargo stuff

---
+++ popsicle/target/debug/deps/popsicle-e8ede8ae8ab5ab2f.d
@@ -0,0 +1,5 @@
+/home/ethan/github/LearningRust/popsicle/target/debug/deps/popsicle-e8ede8ae8ab5ab2f: src/main.rs
+
+/home/ethan/github/LearningRust/popsicle/target/debug/deps/popsicle-e8ede8ae8ab5ab2f.d: src/main.rs
+
+src/main.rs:
---
+++ popsicle/target/debug/popsicle.d
@@ -0,0 +1,1 @@
+/home/ethan/github/LearningRust/popsicle/target/debug/popsicle: /home/ethan/github/LearningRust/popsicle/src/main.rs
